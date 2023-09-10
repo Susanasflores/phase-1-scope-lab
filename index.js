30220a16dacb4a15d02f3d1363b9e78fd8daeed3
Susana Solis Flores <Suzy.flores@icloud.com>
2023-09-10 19:30:09 -0400

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+     customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+      window.bestCustomer = "not bob";
+
+}
+function overwriteBestCustomer() {
+    window.bestCustomer = "maybe bob"
+}
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = "Susana"
+    leastFavoriteCustomer = "Joe"
+}
